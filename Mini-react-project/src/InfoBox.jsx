319d8198228462d0f234439f0071eaa4743844cf
Harsh mishra <harshmishra7542@email.com>
2025-11-21 03:50:26 +0530

    Change Weather App

--- Mini-react-project/src/InfoBox.jsx
+++ Mini-react-project/src/InfoBox.jsx
@@ -34,7 +34,7 @@ export default function InfoBox({info}){
            <p>Min Temp = {info.tempMin}&deg;C</p>
            <p>Max Temp = {info.tempMax}&deg;C</p>
            <p>Humidity = {info.humidity}&deg;C</p>
-           <p>The weather can be described as <i>{info.weather}</i> and feels like {info.feelslike}&deg;C</p>
+           <p>The weather can be described as <i>{info.weather}</i> and feels like {info.feelsLike}&deg;C</p>
         </Typography>
       </CardContent>
      </Card>
